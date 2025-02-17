0651ceb54a92295d40a04048792ef25bdc0c7aee
Jae Hyun Ahn <ahnj233@newschool.edu>
2025-02-17 10:15:17 -0500

    top button

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+//footer #top button 
+document.addEventListener("DOMContentLoaded", function () {
+	const topButton = document.getElementById("top");
+
+	window.addEventListener("scroll", function () {
+		const scrollPosition = window.scrollY;
+
+		if (scrollPosition > 800) {
+			topButton.style.opacity = "1";
+			topButton.style.visibility = "visible";
+		} else {
+			topButton.style.opacity = "0";
+			topButton.style.visibility = "hidden";
+		}
+	});
+});
+
